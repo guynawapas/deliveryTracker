aec12438dbd79f2cb4886015116c473dc08a4eb3
guynawapas <guynawapas@gmail.com>
2020-07-11 22:26:59 +0700

    order filter added

--- src/app/pages/orders/orders.page.ts
+++ src/app/pages/orders/orders.page.ts
@@ -10,21 +10,29 @@ import { isNgTemplate } from '@angular/compiler';
 })
 export class OrdersPage implements OnInit {
   orders:Order[];
- 
+  nearOrders:Order[]=[];
   constructor(private orderService: OrderService, private nav:NavController) { }
 
   ngOnInit() {
     this.orderService.getOrders().subscribe(res =>{
-      this.orders=res;
+       this.orders=res;
+       //console.log(this.orders);
+       //filter called here cus it broke when call outside...
+      this.filterOrders();
+       //console.log(this.nearOrders);
+
     });
-
-   
-
-
-
-
+    
   }
-  
+  //filter Order that is near us (0,0)
+  filterOrders(){
+    for(let i=0;i<this.orders.length;i++){
+      if(this.orders[i].lat<5&&this.orders[i].long<5){
+        this.nearOrders.push(this.orders[i]);
+      }
+    }
+  }
+
   remove(item){
     this.orderService.removeOrder(item.id);
   }
